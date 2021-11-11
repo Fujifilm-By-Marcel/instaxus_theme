6db668dfafcf7f97d7cfaacecb321d5345c7d433
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-11-11 13:20:58 -0500

    canadas privacy policy

--- page-privacy-policy.php
+++ page-privacy-policy.php
@@ -11,9 +11,15 @@ get_header();
 	}
 </style>
 <?php
+if( get_current_blog_id() == 1){
+	$url = 'https://www.fujifilm.com/us/en/privacy';
+}
+else if( get_current_blog_id() == 4){
+	$url = 'https://www.fujifilm.com/ca/en/privacy';
+}
 include('fnac/simplehtmldom_1_9_1/simple_html_dom.php');
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://www.fujifilm.com/us/en/privacy");
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $output = curl_exec($ch);
