82d5d4959a7e4ad3de54d7d993ac0d40a89f4258
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2022-01-20 13:24:28 -0500

    fixed fb code

--- layout/header/header_03.php
+++ layout/header/header_03.php
@@ -1,6 +1,34 @@
 <!-- HEADER
 ================================================== -->
+<?php 
+if(get_current_blog_id() == 1){
+  $fbid = 957675440961709;
+} elseif (get_current_blog_id() == 4){
+  $fbid = 521279755958873;
+} else {
+    $fbid = false;
+}
 
+if ( $fbid ) { 
+?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '<?php echo $fbid; ?>');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=<?php echo $fbid; ?>&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<?php } ?>
 <?php $logancee_options = logancee_get_options();?>
 
 <?php if($logancee_options['header-sticky-status']):?>
